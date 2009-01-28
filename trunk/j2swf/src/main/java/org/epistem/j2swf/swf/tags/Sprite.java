c5d067132892dd632599cba114b9fd127daf5e14
david.nick.main <david.nick.main@8fd6b58c-9eb6-11dd-93a9-677c3c677ea2>
2009-01-28 04:12:23 +0000

    fix compile errors to keep John Brandle quiet

--- trunk/j2swf/src/main/java/org/epistem/j2swf/swf/tags/Sprite.java
+++ trunk/j2swf/src/main/java/org/epistem/j2swf/swf/tags/Sprite.java
@@ -1,8 +1,12 @@
 package org.epistem.j2swf.swf.tags;
 
+import java.io.IOException;
+
 import org.epistem.j2swf.swf.ControlTag;
 import org.epistem.j2swf.swf.DefinitionTag;
 import org.epistem.j2swf.swf.Timeline;
+
+import com.anotherbigidea.flash.interfaces.SWFTagTypes;
 
 /**
  * A sprite definition
@@ -15,4 +19,10 @@ public class Sprite extends DefinitionTag {
      * The sprite timeline
      */
     public final Timeline<ControlTag> timeline = new Timeline<ControlTag>();
+
+    /** @see org.epistem.j2swf.swf.Tag#write(com.anotherbigidea.flash.interfaces.SWFTagTypes) */
+    @Override
+    protected void write( SWFTagTypes tags ) throws IOException {
+        // TODO Auto-generated method stub        
+    }
 }

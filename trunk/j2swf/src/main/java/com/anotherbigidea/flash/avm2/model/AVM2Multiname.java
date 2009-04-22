d402050b693b1f7408819ae6ba76b2045d879ea2
david.nick.main@gmail.com <david.nick.main@gmail.com@8fd6b58c-9eb6-11dd-93a9-677c3c677ea2>
2009-04-22 14:19:25 +0000

    new vararg constructor

--- trunk/j2swf/src/main/java/com/anotherbigidea/flash/avm2/model/AVM2Multiname.java
+++ trunk/j2swf/src/main/java/com/anotherbigidea/flash/avm2/model/AVM2Multiname.java
@@ -1,5 +1,6 @@
 package com.anotherbigidea.flash.avm2.model;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.anotherbigidea.flash.avm2.MultiNameKind;
@@ -17,5 +18,13 @@ public class AVM2Multiname extends AVM2Name {
      */
     public AVM2Multiname( String name, List<AVM2Namespace> namespaceSet ) {
         super( MultiNameKind.Multiname, null, name, namespaceSet );
+    }
+    
+    /**
+     * @param name the simple name
+     * @param namespaces the set of namespaces
+     */
+    public AVM2Multiname( String name, AVM2Namespace... namespaces ) {
+        super( MultiNameKind.Multiname, null, name, Arrays.asList( namespaces ));
     }
 }
